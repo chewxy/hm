6b26671f8cd039db0d04d30d3d57f228eebd0f67
chewxy <chewxy@gmail.com>
2017-12-27 19:20:09 +1100

    Updated variable naming scheme

--- typeVariable.go
+++ typeVariable.go
@@ -9,7 +9,7 @@ import (
 // TypeVariable is a variable that ranges over the types - that is to say it can take any type.
 type TypeVariable rune
 
-func (t TypeVariable) Name() string { return string(t) }
+func (t TypeVariable) Name() string { return fmt.Sprintf("%v", t) }
 func (t TypeVariable) Apply(sub Subs) Substitutable {
 	if sub == nil {
 		return t
@@ -30,7 +30,13 @@ func (t TypeVariable) Normalize(k, v TypeVarSet) (Type, error) {
 	return nil, errors.Errorf("Type Variable %v not in signature", t)
 }
 
-func (t TypeVariable) Types() Types               { return nil }
-func (t TypeVariable) String() string             { return string(t) }
-func (t TypeVariable) Format(s fmt.State, c rune) { fmt.Fprintf(s, "%c", rune(t)) }
-func (t TypeVariable) Eq(other Type) bool         { return other == t }
+func (t TypeVariable) Types() Types   { return nil }
+func (t TypeVariable) String() string { return fmt.Sprintf("%v", t) }
+func (t TypeVariable) Format(s fmt.State, c rune) {
+	if t >= 'a' && t <= 'z' {
+		fmt.Fprintf(s, "%c", t)
+		return
+	}
+	fmt.Fprintf(s, "<%d>", t)
+}
+func (t TypeVariable) Eq(other Type) bool { return other == t }
