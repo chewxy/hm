44a2cf0f344aeb1c32a065e90d674546e2583600
chewxy <chewxy@gmail.com>
2017-12-28 19:36:44 +1100

    Implemented Type for the Pair types. Except for Normalize

--- types/pairs.go
+++ types/pairs.go
@@ -1,5 +1,17 @@
 package hmtypes
 
+import (
+	"fmt"
+
+	"github.com/chewxy/hm"
+)
+
+var (
+	_ hm.Type = &Choice{}
+	_ hm.Type = &Super{}
+	_ hm.Type = &Application{}
+)
+
 // pair types
 
 // Choice is the type of choice of algorithm to use within a class method.
@@ -18,9 +30,26 @@ package hmtypes
 //
 // The first type is the key of either the vtable or the name mangled table.
 // The second type is the value of the table.
-//
-// TODO: implement hm.Type
 type Choice Pair
+
+func (t *Choice) Name() string                       { return ":" }
+func (t *Choice) Apply(sub hm.Subs) hm.Substitutable { ((*Pair)(t)).Apply(sub); return t }
+func (t *Choice) FreeTypeVar() hm.TypeVarSet         { return ((*Pair)(t)).FreeTypeVar() }
+func (t *Choice) Format(s fmt.State, c rune)         { fmt.Fprintf(s, "%v : %v", t.A, t.B) }
+func (t *Choice) String() string                     { return fmt.Sprintf("%v", t) }
+
+func (t *Choice) Normalize(k hm.TypeVarSet, v hm.TypeVarSet) (hm.Type, error) {
+	panic("not implemented")
+}
+
+func (t *Choice) Types() hm.Types { return ((*Pair)(t)).Types() }
+
+func (t *Choice) Eq(other hm.Type) bool {
+	if ot, ok := other.(*Choice); ok {
+		return ot.A.Eq(t.A) && ot.B.Eq(t.B)
+	}
+	return false
+}
 
 // Super is the inverse of Choice. It allows for supertyping functions.
 //
@@ -28,9 +57,26 @@ type Choice Pair
 // But there needs to be a separate accounting structure to keep account of the types.
 //
 // This is where Super comes in.
-//
-// TODO: implement hm.Type
 type Super Pair
+
+func (t *Super) Name() string                       { return "§" }
+func (t *Super) Apply(sub hm.Subs) hm.Substitutable { ((*Pair)(t)).Apply(sub); return t }
+func (t *Super) FreeTypeVar() hm.TypeVarSet         { return ((*Pair)(t)).FreeTypeVar() }
+func (t *Super) Format(s fmt.State, c rune)         { fmt.Fprintf(s, "%v §: %v", t.A, t.B) }
+func (t *Super) String() string                     { return fmt.Sprintf("%v", t) }
+
+func (t *Super) Normalize(k hm.TypeVarSet, v hm.TypeVarSet) (hm.Type, error) {
+	panic("not implemented")
+}
+
+func (t *Super) Types() hm.Types { return ((*Pair)(t)).Types() }
+
+func (t *Super) Eq(other hm.Type) bool {
+	if ot, ok := other.(*Super); ok {
+		return ot.A.Eq(t.A) && ot.B.Eq(t.B)
+	}
+	return false
+}
 
 // Application is the pre-unified type for a function application.
 // In a simple HM system this would not be needed as the type of an
@@ -39,6 +85,23 @@ type Super Pair
 //
 // In advanced systems where unification may be done concurrently, this would
 // be required, as a "thunk" of sorts for the type system.
-//
-// TODO: implement hm.Type
 type Application Pair
+
+func (t *Application) Name() string                       { return "•" }
+func (t *Application) Apply(sub hm.Subs) hm.Substitutable { ((*Pair)(t)).Apply(sub); return t }
+func (t *Application) FreeTypeVar() hm.TypeVarSet         { return ((*Pair)(t)).FreeTypeVar() }
+func (t *Application) Format(s fmt.State, c rune)         { fmt.Fprintf(s, "%v • %v", t.A, t.B) }
+func (t *Application) String() string                     { return fmt.Sprintf("%v", t) }
+
+func (t *Application) Normalize(k hm.TypeVarSet, v hm.TypeVarSet) (hm.Type, error) {
+	panic("not implemented")
+}
+
+func (t *Application) Types() hm.Types { return ((*Pair)(t)).Types() }
+
+func (t *Application) Eq(other hm.Type) bool {
+	if ot, ok := other.(*Application); ok {
+		return ot.A.Eq(t.A) && ot.B.Eq(t.B)
+	}
+	return false
+}
