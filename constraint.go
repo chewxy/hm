82062404ad5f2614a4b17da1d4c0a7c464e667f4
chewxy <chewxy@gmail.com>
2018-03-08 12:03:07 +1100

    Fixed bug.

--- constraint.go
+++ constraint.go
@@ -5,6 +5,6 @@ import "fmt"
 // A Constraint is well.. a constraint that says a must equal to b. It's used mainly in the constraint generation process.
 type Constraint Pair
 
-func (c Constraint) Apply(sub Subs) Substitutable   { (*Pair)(&c).Apply(sub); return c }
+func (c Constraint) Apply(sub Subs) Substitutable   { return Constraint(*(*Pair)(&c).Apply(sub)) }
 func (c Constraint) FreeTypeVar() TypeVarSet        { return Pair(c).FreeTypeVar() }
 func (c Constraint) Format(state fmt.State, r rune) { fmt.Fprintf(state, "{%v = %v}", c.A, c.B) }
